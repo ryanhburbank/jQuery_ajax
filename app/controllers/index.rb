70dfeddbe5dafb2125f0339ab2b707ea93bbf269
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-25 10:53:51 -0500

    Finish Simon Says

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -9,4 +9,5 @@ post '/color' do
 
   cell= rand(1..9)
   color= "#" + "%06x" % (rand * 0xffffff)
+   {color: color, cell: cell }.to_json
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
+  $('#get_color').on('click', function(game){
 
+    game.preventDefault();
+    game.stopPropagation();
+
+    var url = ('/color');
+    
+    $.post(url, function(simonSays){
+    var grid = JSON.parse(simonSays);
+    var gridTile = grid.cell;
+    var color = grid.color;
+    
+    console.log(color);
+    console.log(gridTile);
+    
+    $('#color_me li:nth-child('+ gridTile +')').css('background-color', color);
+  });
+  });
 });
